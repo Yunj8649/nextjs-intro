d644cdf05bfb5e33e897776b96abcb017c8c02f1
yunj <yunj8649@xingxingmobility.com>
2022-02-01 20:08:29 +0900

    2.3 Server Side Rendering

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,11 @@ import styles from '../styles/Home.module.css'
 import NavBar from '../components/NavBar';
 import Seo from '../components/Head';
 
-export default function Home() {
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -46,4 +38,14 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+// client가 아니라 server 쪽에서만 작동한다
+export async function getServerSideProps() {
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+  return {
+    props: {
+      results
+    }
+  }
 }
